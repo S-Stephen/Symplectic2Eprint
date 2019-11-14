c3e66235840bbfa4f41ab792afe2c00032c5cea3
StephenS <Stephen.shorrock@gmail.com>
2019-11-14 14:49:51 +0000

    issue retrieving from port 8092?

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 from lxml import etree
- 
+import urllib.request 
+import argparse
+
+
+
+
+parser = argparse.ArgumentParser(description='credentials for request')
+parser.add_argument('credentials', help='password')
+args = parser.parse_args()
+
+relurl = "https://"+args.credentials+"@elements.admin.cam.ac.uk:8092/elements-api/v5.5/relationship/2002783"
+print(relurl)
+
+fp = urllib.request.urlopen(relurl)
+mybytes = fp.read()
+
+'''
+mystr = mybytes.decode("utf8")
+fp.close()
+
+print(mystr)
+
 data = open('./symplectic2eprint.xslt')
 xslt_content = data.read()
 xslt_root = etree.XML(xslt_content)
@@ -11,3 +32,4 @@ print(str(result))
 #f.write(str(result1))
 #f.close()
 
+'''
